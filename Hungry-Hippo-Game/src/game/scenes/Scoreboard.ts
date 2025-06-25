cdb4018480f1289be269d1aa9af6fda9d3db6f9a
Arvindh Velrajan <tun67213@temple.edu>
2025-06-25 11:02:45 -0400

    Implemented constructor

--- Hungry-Hippo-Game/src/game/scenes/Scoreboard.ts
+++ Hungry-Hippo-Game/src/game/scenes/Scoreboard.ts
@@ -10,4 +10,21 @@ export class Scoreboard
     private playerScores : Record<string, number> = {};
     private scoreText: Phaser.GameObjects.Text;
     
+    constructor(scene: Phaser.Scene, x = 32, y = 32)
+    {
+        this.scene = scene;
+
+        this.scoreText = this.scene.add.text(x, y, '', 
+            {
+                fontSize: '24px',
+                color: '#000',
+                fontFamily: 'Arial',
+                align: 'left',
+                backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                padding: { x: 10, y: 10}
+            }
+        );
+
+        this.updateScoreText();
+    }
 }
